b3ce5b2ad72878792bbc488524a256b689437aad
Maksim Lekomtsev <lekomtsev@unix-mastery.ru>
2021-02-03 15:25:44 +0400

    [build_ubuntu_livecd.sh]: Moved helper (future shared) functions in a separate file (refs #7)

--- build_ubuntu_livecd.sh
+++ build_ubuntu_livecd.sh
@@ -13,109 +13,20 @@ UBUNTU_ISO_PATH="${UBUNTU_ISO_PATH:-ubuntu-${UBUNTU_SUITE}-${UBUNTU_ARCH}-live-v
 UBUNTU_RUN_OPTIONS="${UBUNTU_RUN_OPTIONS:-textonly toram vga=792}"
 
 set -o errexit
-trap internal ERR
 
-function _print {
-  local print_type="${1}"
-  shift
-
-  if [[ -z "${1}" ]]
-  then
-    return 0
-  fi
-
-  case "${print_type}" in
-    "info" )
-      local info_message
-      for info_message in "${@}"; do
-        printf -- "!!! ${info_message}\n"
-      done
-      echo
-      ;;
-    "progress" )
-      local progress_message
-      for progress_message in "${@}"; do
-        printf -- "--> %i. %s ...\n" $((_progress_number)) "${progress_message}"
-        let _progress_number+=1
-      done
-      ;;
-    * )
-      {
-        local ident="!!! ${print_type^^}:"
-        local ident_length="${#ident}"
-        echo
-        local error_message
-        for error_message in "${@}"; do
-          printf -- "%-${ident_length}s %s\n" "${ident}" "${error_message}"
-          ident=""
-        done
-        echo
-      } >&2
-      ;;
-  esac
-}
-
-function error {
-  _print error "${@}"
+my_dir="${0%/*}"
+if ! source "${my_dir}"/functions.sh.inc 2>/dev/null
+then
+  echo "!!! ERROR: Can't load a functions file (functions.sh.inc)"
+  echo "           Please check archive of this script or use 'git checkout --force' command if it cloned from git"
   exit 1
-}
+fi
 
-function warning {
-  _print warning "${@}"
-  exit 0
-}
-
-function info {
-  _print info "${@}"
-}
-
-function progress {
-  _print progress "${@}"
-}
-
-function internal {
-  if [ -z "${FUNCNAME[2]}" ]
-  then
-    _print shit \
-      "Happen in '${FUNCNAME[1]}' function call at ${BASH_LINENO[0]} line" \
-      "Let the developer know or solve the problem yourself"
-  else
-    _print internal \
-      "Problem in '${FUNCNAME[2]}' -> '${FUNCNAME[1]}' function call at ${BASH_LINENO[1]} line:" \
-      "${@}"
-  fi
-  exit 2
-}
-
-function check_commands {
-  while [ -n "${1}" ]
-  do
-    local required_command="${1}"
-    if ! type -P "${required_command}" >/dev/null
-    then
-      error \
-        "The required command '${required_command}' is not exist" \
-        "Please check your PATH environment variable" \
-        "And install a required command through your package manager"
-    fi
-    shift
-  done
-}
-
-function check_root_run {
-  if [ ${EUID} -gt 0 ]
-  then
-    error \
-      "For properly run this script, it's should be runned by ROOT user only" \
-      "You can use 'sudo' command for this"
-  fi
-}
+trap internal ERR
 
 echo "Script for build Ubuntu LiveCD v${MY_VERSION}"
 echo "suite:\"${UBUNTU_SUITE}\" arch:\"${UBUNTU_ARCH}\" output_iso_path:\"${UBUNTU_ISO_PATH}\""
 echo
-
-let _progress_number=1
 
 if [ -s "${UBUNTU_ISO_PATH}" ]
 then
@@ -127,14 +38,11 @@ progress "Checking requirements"
 check_commands \
   cat \
   debootstrap \
-  dirname \
   mkisofs \
   mksquashfs \
   mktemp \
-  realpath
 
 progress "Checking required files"
-my_dir=$(dirname $(realpath "${0}"))
 for f in \
   "${my_dir}"/isolinux/isolinux.bin \
   "${my_dir}"/isolinux/ldlinux.c32
