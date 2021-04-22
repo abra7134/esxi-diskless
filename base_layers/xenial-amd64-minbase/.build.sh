7330f5ce4d52b907af88c27d5da47f1e6b76fefa
Maksim Lekomtsev <lekomtsev@unix-mastery.ru>
2021-04-22 12:08:15 +0400

    [base_layers/xenial-amd64-minbase]: The first version (refs #54)

---
+++ base_layers/xenial-amd64-minbase/.build.sh
@@ -0,0 +1,130 @@
+#!/usr/bin/env sh
+
+set -o errexit
+
+build_dir="${1}"
+
+if [ ! -d "${build_dir}" ]
+then
+  echo >&2 "    The build '${build_dir}' directory is not exists, aborting"
+  exit 1
+fi
+
+function unmount_orphans {
+  local \
+    i="" \
+    fs_name=""
+
+  for i in \
+    proc/loadavg \
+    sys/power/state
+  do
+    if [ -f "${build_dir}/${i}" ]
+    then
+      fs_name="${i%%/*}"
+      echo "-- Unmount a pseudo filesystem '${fs_name}' from '${build_dir}'"
+      umount \
+        --verbose \
+        "${build_dir}/${fs_name}"
+    fi
+  done
+
+  return 0
+}
+
+trap unmount_orphans ERR
+trap unmount_orphans SIGINT
+
+for required_command in \
+  cp \
+  debootstrap \
+  mkdir \
+  patch \
+  rm
+do
+  if ! type -P "${required_command}" >/dev/null
+  then
+    echo "!!"
+    echo "!! The required command '${required_command}' is not exist"
+    echo "!!"
+    exit 1
+  fi
+done
+
+echo "--"
+echo "-- Bootstrapping an Ubuntu 'xenial' minbase variant with 'casper' livecd hooks (debootstrap)"
+echo "-- in '${build_dir}' directory"
+echo "--"
+debootstrap \
+  --arch=amd64 \
+  --include=apt-utils,casper,console-setup,ifupdown,linux-virtual,open-vm-tools,resolvconf,ssh \
+  --variant=minbase \
+  xenial \
+  "${build_dir}" \
+  http://archive.ubuntu.com/ubuntu
+
+unmount_orphans
+
+echo "--"
+echo "-- Cleanup unnecessary 'casper' scripts (rm)"
+echo "--"
+for f in \
+  07remove_oem_config \
+  13swap \
+  15autologin \
+  18hostname \
+  20xconfig \
+  22desktop_settings \
+  23networking \
+  24preseed \
+  25adduser \
+  25disable_cdrom.mount \
+  26serialtty \
+  30accessibility \
+  31disable_update_notifier \
+  33enable_apport_crashes \
+  34disable_kde_services \
+  35fix_language_selector \
+  36disable_trackerd \
+  40install_driver_updates \
+  41apt_cdrom \
+  44pk_allow_ubuntu \
+  45jackd2 \
+  48kubuntu_disable_restart_notifications \
+  49kubuntu_mobile_session \
+  50ubiquity-bluetooth-agent \
+  51unity8_wizard
+do
+  rm \
+    --verbose \
+    "${build_dir}/usr/share/initramfs-tools/scripts/casper-bottom/${f}"
+done
+
+echo "--"
+echo "-- Copying the necessary files (cp)"
+echo "--"
+patch \
+  "${build_dir}"/etc/ssh/sshd_config \
+  etc/ssh/sshd_config.patch
+for f in \
+  etc/sysctl.d/kernel.panic.conf \
+  etc/sysfs.d/10-set_noop_scheduler.conf \
+  etc/systemd/system/cloud-network.service \
+  etc/systemd/system/unmount-cdrom.service \
+  etc/systemd/system/network-online.target.wants/cloud-network.service \
+  etc/systemd/system/sysinit.target.wants/cloud-network.service \
+  etc/systemd/system/sysinit.target.wants/unmount-cdrom.service \
+  opt/cloud-network.sh
+do
+  mkdir \
+    --parents \
+    --verbose \
+    "${build_dir}/${f%/*}"
+  cp \
+    --no-dereference \
+    --verbose \
+    "${f}" \
+    "${build_dir}/${f}"
+done
+
+exit 0
---
+++ base_layers/xenial-amd64-minbase/.pre_image.sh
@@ -0,0 +1,98 @@
+#!/usr/bin/env sh
+
+set -o errexit
+
+build_dir="${1}"
+image_dir="${2}"
+
+ROOT_PASSWORD="examplePassword789"
+
+OPTS_MKSQUASHFS="-no-xattrs"
+# All run options see at http://manpages.ubuntu.com/manpages/xenial/man7/casper.7.html
+OPTS_BOOT_UBUNTU="textonly toram net.ifnames=0 biosdevname=0"
+
+if [ ! -d "${build_dir}" ]
+then
+  echo >&2 "    The build '${build_dir}' directory is not exists, aborting"
+  exit 1
+elif [ ! -d "${image_dir}" ]
+then
+  echo >&2 "    The image '${image_dir}' directory is not exists, aborting"
+  exit 1
+fi
+
+echo "--"
+echo "-- Update a ROOT password (mkpasswd & sed)"
+echo "--"
+root_password_encrypted=$(
+  mkpasswd \
+    -m sha-512 \
+    "${ROOT_PASSWORD}"
+)
+sed --in-place \
+  "/^root:/s|\*|${root_password_encrypted}|" \
+  "${build_dir}"/etc/shadow
+
+echo "--"
+echo "-- Create necessary directories (mkdir)"
+echo "--"
+mkdir \
+  --parents \
+  --verbose \
+  "${image_dir}/casper"
+
+echo "--"
+echo "-- Cleanup unnecessary files before a squashing (rm)"
+echo "--"
+rm \
+  --recursive \
+  --verbose \
+  "${build_dir}"/usr/share/man/?? \
+  "${build_dir}"/usr/share/man/??_* \
+  "${build_dir}"/usr/share/locale/* \
+  "${build_dir}"/var/cache/apt/archives/*.deb \
+  "${build_dir}"/var/lib/apt/lists/*
+
+echo "--"
+echo "-- Update the initramfs image (update-initramfs)"
+echo "--"
+chroot \
+  "${build_dir}" \
+  /bin/bash -c 'export PATH=/bin:/sbin:/usr/bin:/usr/sbin LANG=C; update-initramfs -u'
+
+echo "--"
+echo "-- Squashing a filesystem tree (mksquashfs)"
+echo "--"
+mksquashfs \
+  "${build_dir}"/ \
+  "${image_dir}"/casper/filesystem.squashfs \
+  ${OPTS_MKSQUSHFS} \
+  -e boot/
+
+echo "--"
+echo "-- Adding a kernel and initrd to LiveCD tree (cp)"
+echo "--"
+for f in \
+  config \
+  initrd.img \
+  vmlinuz
+do
+  cp --verbose \
+    "${build_dir}"/boot/${f}-* \
+    "${image_dir}"/casper/${f}
+done
+
+echo "--"
+echo "-- Preparing a isolinux boot loader configuration"
+echo "--"
+cat \
+> "${image_dir}"/isolinux/isolinux.cfg \
+<<EOF
+DEFAULT live
+LABEL live
+  kernel /casper/vmlinuz
+  append boot=casper initrd=/casper/initrd.img ${OPTS_BOOT_UBUNTU} --
+PROMPT 0
+EOF
+
+exit 0
